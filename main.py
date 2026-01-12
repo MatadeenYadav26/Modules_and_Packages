a8772e7f7a15907ed1e25d1ad1025ab8d0fb628d
KING <matadeenyadav202@gmail.com>
2026-01-12 08:07:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#creating a module :
+#using modules from : math_utils.py
+
+# import math_utils
+
+# print(math_utils.add(5,6))
+# print(math_utils.sub(9,6))
+
+
+# from math_utils import add , sub 
+
+# or  for using all the functions of a module use :
+# from math_utils import *
+# print(add(5,10))
+# print(sub(5,10))
+
+
+#built in modules : import - math , random , string
+
+# from my_packages import string_utils , math_utils
+
+# print(string_utils.upper('a'))
+# print(math_utils.add(15,15))
+
+# from my_packages.math_utils import add
+# from my_packages.string_utils import upper
